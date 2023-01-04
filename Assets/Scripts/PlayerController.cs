fb2b3e2b058f98545f16c1737c9d91e87ae6d93a
DjTaLDjmS <sdemarco@hotmail.fr>
2023-01-04 11:56:58 +0100

    Lesson 2.1 - Player Positioning - 6.Clean up your code and variables

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,9 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float horizontalInput;
     [SerializeField]
-    private float speed = 10.0f;
+    private float speed = 10.0f; 
+    [SerializeField]
+    private float xRange = 10.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,9 +20,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < -10)
+        // Keep the player in bounds
+        if (transform.position.x < -xRange)
         {
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
