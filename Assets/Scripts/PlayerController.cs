88a75217c57b71dce8360f3a762aba99fc64d28d
DjTaLDjmS <sdemarco@hotmail.fr>
2023-01-05 15:34:26 +0100

    Replacing old InputSystem by new InputSystem

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,23 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    private float horizontalInput;
-    [SerializeField]
-    private float speed = 10.0f; 
+    private float speed = 10.0f;
     [SerializeField]
     private float xRange = 10.0f;
 
     [SerializeField]
     private GameObject projectilePrefab;
 
+    private float movementX;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -32,13 +33,23 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
-    
-        if (Input.GetKeyDown(KeyCode.Space))
+        transform.Translate(Vector3.right * movementX * Time.deltaTime * speed);
+
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Vector2 vect = context.ReadValue<Vector2>();
+        movementX = vect.x;
+    }
+
+    public void OnFire(InputAction.CallbackContext context)
+    {
+        if (context.started)
         {
             //Launch a projectile from the player
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
+
     }
 }
