5a880f61406b76d2e05902656a8091cb9ada5644
DjTaLDjmS <sdemarco@hotmail.fr>
2023-01-09 15:57:04 +0100

    Modification of the camera angle to 1st person view Xpos 20 to 10

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,7 +7,7 @@ public class SpawnManager : MonoBehaviour
     [SerializeField]
     private GameObject[] animalPrefabs;
 
-    private float spawnRangeX = 20;
+    private float spawnRangeX = 10;
     private float spawnPosZ = 20;
 
     private float startDelay = 2;
